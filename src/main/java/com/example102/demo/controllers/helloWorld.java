314f9df760e62d3a8165d1f9db5647fb4f9c5bb9
Santiago Alonzo Aguilar <salonzo9878@gmail.com>
2026-03-04 08:33:08 -0600

    quick fix

--- src/main/java/com/example102/demo/controllers/helloWorld.java
+++ src/main/java/com/example102/demo/controllers/helloWorld.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-public class hellowrold {
+public class helloWorld {
     @Autowired
     private HelloWorldServices service;
 
     @GetMapping("/")
-    public String getMethodName(@RequestParam String name){
+    public String getMethodName(@RequestParam String name) {
         return service.generateMessage(name);
     }
 }
